b731fc186a7fa4273a973fcb5978bc09d7188fae
Cedric GESTES <ctaf42@gmail.com>
2007-02-17 02:51:03 +0000

    colorgrep

--- bin/colorgrep.pl
+++ bin/colorgrep.pl
@@ -56,44 +56,9 @@ while (<>)
 {
     $orgline = $thisline = $_;
 
-    # Remove multiple spaces
-    $thisline =~ s/  \+/ /g;
-
-    #make[1] | make:
-    $thisline =~ s/^((p|g)?make)(\[[1234567890]*\]|:)(.*$)/$col_make$1$col_norm$col_brown$3$col_trace$4$col_default/x;
-    $thisline =~ s/^(\s*(g?cc|(g|c)\+\+))/$col_gcc$1$col_default/x;
-#     $thisline =~ s/^(.*(libtool\s)*.*(g?cc|(g|c)\+\+))$/$col_gcc$1$col_norm/x;
-#     $thisline =~ s/^(((\t|\s)*then\s))$/$col_gcc$1$col_norm/x;
-#    $thisline =~ s/^(\s*\(|\[|a(r|wk)|c(p|d|h(mod|own))|do(ne)?|e(cho|lse)|f(ind|or)|i(f|nstall)|mv|perl|r(anlib|m(dir)?)|s(e(d|t)|trip)|tar)\s+/$col_gcc$1 $col_default/x;
-
-#     $thisline =~ s/(In (member\s)+f(unction|ile))/$col_trace$1$col_normal/x;
-
-    #filename: In function:
-    $thisline =~ s/^(.*:\s*)(In\s|At\s)(.*)/$col_filename$1$col_trace$2$3$col_default/x;
-
-    #filename: 123: error: ...
-    $thisline =~ s|^(.*:\s*)(\d+\s*:\s*)(error\s*:\s+)(.*)|$col_filename$1$col_linenum$2$col_underline$col_error$3$col_norm$col_purple$4$col_default|x;
-    #filename: 123: warning: ...
-    $thisline =~ s|^(.*:\s*)(\d+\s*:\s*)(warning\s*:\s+)(.*)|$col_filename$1$col_linenum$2$col_underline$col_warning$3$col_norm$col_brighten$col_purple$4$col_default|x;
-
+    $thisline =~ s|^(.*:\s*)(\d+\s*:\s*)(.*)|$col_filename$1$col_linenum$2$col_default$3|x;
     print $thisline;
 }
 
 print $col_default;
-
-# UNUSED:
-#
-#%colors = (
-#    'black'     => "\033[30m",
-#    'red'       => "\033[31m",
-#    'green'     => "\033[32m",
-#    'yellow'    => "\033[33m",
-#    'blue'      => "\033[34m",
-#    'magenta'   => "\033[35m",
-#    'purple'    => "\033[35m",
-#    'cyan'      => "\033[36m",
-#    'white'     => "\033[37m",
-#    'darkgray'  => "\033[30m");
-
-
 
