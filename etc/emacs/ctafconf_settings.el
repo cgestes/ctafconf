76d39536876b070b00c75fbde7018e9bc1f00093
Cedric GESTES <ctaf42@gmail.com>
2010-06-18 16:25:24 +0200

    emacs: enable eldoc for lisp buffer (display help about func/var)

--- etc/emacs/ctafconf_settings.el
+++ etc/emacs/ctafconf_settings.el
@@ -189,6 +189,8 @@
 ;;     (sr-speedbar-open))
 
 ;;display function's doc in the minibuffer for elisp
-(require 'eldoc)
-
+;;(require 'eldoc)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
 
