d7b7749419082bd3ef6e0cff2d2ea0abc3b5b3c7
Cedric Gestes <cedric.gestes@tanker.io>
2017-02-13 11:47:57 +0100

    refactor: env/var, remove shelltools.sh

--- etc/users/user-profile.sh
+++ etc/users/user-profile.sh
@@ -2,19 +2,19 @@
 #CTAFCONF configuration file
 
 #set your name here (for git, emacs, ...)
-var_set ctafconf_name ''
+ctafconf_name=''
 
 #set your mail here (for git, emacs, ...)
-var_set ctafconf_mail ''
+ctafconf_mail=''
 
 #set your prefered editor here (emacs, nano, vim, vi)
-var_set ctafconf_editor 'emacs'
+ctafconf_editor='emacs'
 
 #list of specific configurations (ctaf, yannick, aldebaran)
 #if you dont know => dont change this variables
 #you can list availables profiles in .config/ctafconf/profile
-var_set ctafconf_profiles ''
+ctafconf_profiles=''
 
 #choose your zsh prompt
 #choice are: ctaf adam1 adam2 bart zefram fade redhat suse walters bigfade clint elite elite2 fire off olivier
-var_set ctafconf_zprompt 'ctaf'
+ctafconf_zprompt='ctaf'
